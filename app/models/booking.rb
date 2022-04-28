2f45de0f3d03793d18cccdce7113abf1975a1c81
Khadija Bulhan <kbulhan8@gmail.com>
2022-04-28 20:21:28 +1000

    validations and associations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
+
+  validates :description, :venue, :music_genre, :event_type, :date, :set_length, :hourly_rate, presence: true
+  validates :description, length: { min: 50 }, presence: true
 end
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,4 @@
 class Playlist < ApplicationRecord
   belongs_to :user
+   
 end
