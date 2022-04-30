d5b17dc8ac1eba3d5109f7d50d6a5fcd4ce98364
Rachael McNamara <rachaelmcnamaraa@gmail.com>
2022-04-30 16:28:07 +1000

    testing somehting...

--- config/application.rb
+++ config/application.rb
@@ -5,20 +5,3 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
-module BITEJockey
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-  end
-
-  RSpotify::authenticate("9727e183cc694c2692b3621c7c9a7c57", ENV['SPOTIFY_SECRET_KEY'])
-end
