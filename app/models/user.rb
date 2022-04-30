76517aa41b98450179809f66f232b06afeb41c35
Rachael McNamara <rachaelmcnamaraa@gmail.com>
2022-04-30 15:18:12 +1000

    heroku config

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
-
-  #has_many :bookings, :playlists
-=======
   # has_many :bookings, :playlists
->>>>>>> 94498ab26f366c1667447c09c4f50538759f1667
   validates :first_name, :last_name, presence: true
 end
