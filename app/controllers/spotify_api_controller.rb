429f4f59ecdd5cb1b6f22e7c3281d290dab22c5e
Shane Garland <shane.garland1@gmail.com>
2022-05-01 12:00:40 +1000

    Redirect to playlist page on connection

--- app/controllers/spotify_api_controller.rb
+++ app/controllers/spotify_api_controller.rb
@@ -3,12 +3,16 @@ class SpotifyApiController < ApplicationController
   end
 
   def success
-    spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    @hash = spotify_user.to_hash
+    spotify_user = RSpotify::User.new(request.env['omniauth.auth']).to_hash
+    redirect_to playlists_path(spotify_user: spotify_user)
   end
 
   def playlists
     spotify_user = RSpotify::User.new(params[:spotify_user])
     @playlists = spotify_user.playlists
+  end
+
+  def playlist_tracks
+
   end
 end
