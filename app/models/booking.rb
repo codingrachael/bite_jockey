46160e7b0dd4bf7082d02c5a08e26f29fcba8a6d
Khadija Bulhan <kbulhan8@gmail.com>
2022-05-07 16:56:39 +1000

    ap/search/booking-unfinished

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,8 @@ class Booking < ApplicationRecord
 
   validates :description, :venue, :music_genre, :event_type, :date, :set_length, :hourly_rate, presence: true
   validates :description, length: { minimum: 50 }, presence: true
+
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :bookings
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
@@ -12,4 +13,6 @@ Rails.application.routes.draw do
   get 'social', to: 'pages#social_home'
 
   get 'playlist-tracks', to: 'spotify_api#playlist_tracks'
+  
+  get 'social_index', to: 'pages#social_index'
 end
