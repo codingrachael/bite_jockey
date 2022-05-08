c66e4724079b5e2fe0d15c403b98c70dd9fedd2f
Tim Fawcett <timfawcett21@gmail.com>
2022-05-08 19:33:27 +1000

    jumbled mess

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,12 @@ Rails.application.routes.draw do
   get 'social', to: 'pages#social_home'
 
   get 'playlist-tracks', to: 'spotify_api#playlist_tracks'
+
+  get "/users", to: "users#index"
+  get '/users/new', to: 'users#new'
+  get '/users/:id', to: 'users#show'
+  post '/users', to: 'users#create'
+  get '/users/:id/edit', to: 'users#edit'
+  patch '/users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
 end
