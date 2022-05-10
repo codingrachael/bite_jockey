f5ee3073fe7da175a626be04709de79f03d51af6
Tim Fawcett <timfawcett21@gmail.com>
2022-05-10 14:57:23 +1000

    nested routes user & bookings

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,18 @@ Rails.application.routes.draw do
 
   get 'playlist-tracks', to: 'spotify_api#playlist_tracks'
 
-  get "/users", to: "users#index"
-  get '/users/new', to: 'users#new'
-  get '/users/:id', to: 'users#show'
-  post '/users', to: 'users#create'
-  get '/users/:id/edit', to: 'users#edit'
-  patch '/users/:id', to: 'users#update'
-  delete 'users/:id', to: 'users#destroy'
+  resources :grandparents do
+    resources :bookings, only: %i[new create edit update index show]
+  end
+
+
+  resources :bookings, only: %i[destroy]
+
+  # get "/users", to: "users#index"
+  # get '/users/new', to: 'users#new'
+  # get '/users/:id', to: 'users#show'
+  # post '/users', to: 'users#create'
+  # get '/users/:id/edit', to: 'users#edit'
+  # patch '/users/:id', to: 'users#update'
+  # delete 'users/:id', to: 'users#destroy'
 end
