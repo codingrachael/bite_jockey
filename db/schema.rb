dc8cf3e15c4cc4568d4a5a3bd177c01a9f155ce3
Tim Fawcett <timfawcett21@gmail.com>
2022-05-18 19:10:59 +1000

    added pronoun, description and genres to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_07_025243) do
+ActiveRecord::Schema.define(version: 2022_05_18_090820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,9 @@ ActiveRecord::Schema.define(version: 2022_05_07_025243) do
     t.string "social_link"
     t.float "latitude"
     t.float "longitude"
+    t.string "pronoun"
+    t.text "description"
+    t.string "genres"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
