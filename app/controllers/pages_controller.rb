d0a2a09b667090f8b8f28eebf07badea8510c73a
Shane Garland <shane.garland1@gmail.com>
2022-05-23 20:55:11 +1000

    Remove un needed code comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,20 +6,5 @@ class PagesController < ApplicationController
   end
 
   def social_index
-    #mock data
-    @markers = [{
-      lat: 37.8238,
-      lng: 144.9913,
-    }]
-      # @users = User.all
-      # # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
-      # @markers = @users.geocoded.map do |user|
-      #   {
-      #     lat: user.latitude,
-      #     lng: user.longitude
-        #  "info_window", locals: { flat: flat }),
-        #  image_url: helpers.asset_url("DJicon.png")
-      #   }
-      # end
-    end
+  end
 end
